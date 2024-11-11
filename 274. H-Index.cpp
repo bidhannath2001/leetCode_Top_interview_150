101e0963afe06e0cee0ea72c2223ce8e1009e08b
Bidhan Nath <117301301+bidhannath2001@users.noreply.github.com>
2024-11-11 21:01:14 +0600

    H-Index

--- 274. H-Index.cpp
+++ 274. H-Index.cpp
@@ -1,4 +1,4 @@
-Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+/*Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
 
 According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
 Example 1:
@@ -19,7 +19,7 @@ n == citations.length
 1 <= n <= 5000
 0 <= citations[i] <= 1000
 
-solution
+solution*/
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
